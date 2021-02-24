f3b9756066137111b0df0c23d9355f1254ad581b
Alejandro Chaves <alejandrochvs@alejandrochvs.com>
2021-02-23 23:04:24 -0500

    react-spring

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {useSpring, animated, interpolate} from 'react-spring';
+import {useGesture} from 'react-with-gesture';
 
-function App() {
+// import {Motion, spring} from 'react-motion';
+
+import './App.scss';
+
+function App({children}) {
+  const [bind, {delta, down}] = useGesture();
+  const {x, bg, size} = useSpring({
+    x: down ? delta[0] : 0,
+    bg: `linear-gradient(120deg, ${delta[0] < 0 ? '#f093fb 0%, #f5576c' : '#96fbc4 0%, #f9f586'} 100%)`,
+    size: down ? 1.1 : 1,
+    immediate: name => down && name === 'x'
+  });
+  const avSize = x.interpolate({
+    map: Math.abs, range: [50, 300],
+    output: ['scale(0.5)', 'scale(1)'],
+    extrapolate: 'clamp'
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <section>
+        <animated.div {...bind()} className="item" style={{background: bg}}>
+          <animated.div className="av" style={{transform: avSize, justifySelf: delta[0] < 0 ? 'end' : 'start'}}/>
+          <animated.div
+            className="fg"
+            style={{transform: interpolate([x, size], (x, s) => `translate3d(${x}px,0,0) scale(${s})`)}}>
+            <a href="https://www.react-spring.io/docs/hooks/basics" target="_blank">React-spring</a>
+          </animated.div>
+        </animated.div>
+      </section>
+      <section>
+        {/*<Motion defaultStyle={{x: 0}} style={{x: spring(10)}}>
+          {value => <div>{value.x}</div>}
+        </Motion>*/}
+      </section>
+      <section></section>
     </div>
   );
 }
